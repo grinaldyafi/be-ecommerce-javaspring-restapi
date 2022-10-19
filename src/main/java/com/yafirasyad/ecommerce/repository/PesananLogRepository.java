1bc5e48d50cd0927146bc9715964bcd52610c637
grinaldyafi <grinaldy.yafi@mail.ugm.ac.id>
2022-10-19 17:15:59 +0700

    merapikan files dan hapus file tidak terpakai

--- src/main/java/com/yafirasyad/ecommerce/repository/PesananLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yafirasyad.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yafirasyad.ecommerce.entity.PesananLog;
-
-public interface PesananLogRepository extends JpaRepository<PesananLog, String> {
-
-}
